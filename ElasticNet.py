0461ae245c8aac6ee972c2dbdd0ab8e326effd9f
WEIYI2021ZHU <zhu.weiy@northeastern.edu>
2023-12-15 01:43:00 -0500

    Adjusting

--- ElasticNet.py
+++ ElasticNet.py
@@ -6,6 +6,7 @@ import yfinance as yf
 from datetime import datetime
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+import scipy.stats as ss
 from sklearn.linear_model import ElasticNet
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
@@ -25,21 +26,27 @@ DATA = DATA_raw.copy()
 DATA['Date'] = pd.to_datetime(DATA['Date'])
 # X and Y have to use the same index and index must be of the same data type
 
+
+
+##Nomalize the Volumn attribute in the model
+#for column in DATA_raw.columns[1:7]:
+ #   DATA[column] = (DATA_raw[column] -
+  #                         DATA_raw[column].mean()) / DATA_raw[column].std()  
+
+#check null values
 ##STOCK_raw = yf.download('LULU', start_date, end_date)
 ##STOCK_raw.describe()
 ##STOCK = STOCK_raw.copy()
-X = DATA.set_index('Date').fillna(method='bfill')
-# y = STOCK['Adj Close']
-y = np.diff(np.log(DATA['Adj Close'].values))
-y = np.append(y[0], y)
+df1 = DATA.iloc[:,0:22]
+df2 = DATA.iloc[:,25:]
+df_concat = pd.concat([df1,df2], axis=1)
+X = df_concat.set_index('Date').fillna(method='bfill')
+y = DATA['Adj Close']
+#y = np.diff(np.log(DATA['Adj Close'].values))
+#y = np.append(y[0], y)
 X = sm.add_constant(X)
 
-##Nomalize the Volumn attribute in the model
-for column in DATA_raw.columns[1:]:
-    DATA[column] = (DATA_raw[column] -
-                           DATA_raw[column].mean()) / DATA_raw[column].std()  
 
-#check null values
 
 #train,test
 X_train, X_test, y_train, y_test = train_test_split(
@@ -47,7 +54,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 
 ##Feature Selection using Elastic Net
-a=1
+a=0.5
 
 
 
@@ -59,7 +66,7 @@ x_train = X_train[model_select]
 model = sm.OLS(y_train,x_train).fit()
 print(model.summary())
 y_pred = model.predict(x_train)
-corr_model = ss.pearsonr(y_pred, y_test)[0]
+corr_model = ss.pearsonr(y_pred, y_train)[0]
 print('model Elastic Net: corr (Y, Y_pred) = '+str(corr_model))
 print('ElasticNet selected ' +str(len(model_select)) +' features: ', model_select.values)
 
